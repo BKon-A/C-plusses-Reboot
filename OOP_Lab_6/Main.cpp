958376c02afe96eadeaae08bcdf800b5f3c1f51b
BKon-A <bogdankonovalenko291@gmail.com>
2022-11-11 11:13:52 +0200

    Update Main.cpp

--- OOP_Lab_6/Main.cpp
+++ OOP_Lab_6/Main.cpp
@@ -24,6 +24,31 @@ public:
 	{
 
 	}
+
+	string getNameOrg()
+	{
+		return nameOfOrganisation;
+	}
+
+	void setNameOrg(string name)
+	{
+		this->nameOfOrganisation = name;
+	}
+
+	float getBudget()
+	{
+		return budget;
+	}
+	void setBudget(float value)
+	{
+		this->budget = value;
+	}
+
+	void showInfoOrganisation()
+	{
+		cout << "Organisation: " << getNameOrg() 
+			<< " have budget: " << getBudget() << "\n";
+	}
 };
 
 class InsuranceProvider : public Organisation
@@ -36,6 +61,7 @@ public:
 	InsuranceProvider()
 	{
 		nameOfDoutherCompanyforInsurance = "WaHYiDM"; // We are help you in difficult moment
+		budgetLosses = 1900000.59;
 	}
 	InsuranceProvider(string name, float losses)
 	{
@@ -46,9 +72,34 @@ public:
 	{
 
 	}
+
+	string getNameInsCompany()
+	{
+		return nameOfDoutherCompanyforInsurance;
+	}
+	void setNameInsCompany(string name)
+	{
+		this->nameOfDoutherCompanyforInsurance = name;
+	}
+
+	float getBudgetLosses()
+	{
+		return budgetLosses;
+	}
+
+	float getInsPaiment()
+	{
+		return InsurancePayment;
+	}
+
+	void showInfoProvider()
+	{
+		cout << "Insurance company: " << getNameInsCompany() 
+			<< " have losses: " << getBudgetLosses() << "\n";
+	}
 };
 
-class BuildingCompany : public Organisation
+class BuildingCompany : public InsuranceProvider
 {
 	string nameOfDoutherCompanyforBuildingCompany;
 	int timeForConstruction; // days
@@ -82,12 +133,40 @@ public:
 			costOfConstructionPerUnitOfTime = 17500;
 		}
 	}
+
+	string getNameBuildingCompany()
+	{
+		return nameOfDoutherCompanyforBuildingCompany;
+	}
+
+	void setNameBuildingCompany(string name)
+	{
+		this->nameOfDoutherCompanyforBuildingCompany = name;
+	}
+
+	int getTime()
+	{
+		return timeForConstruction;
+	}
+	void setTime(int value)
+	{
+		this->timeForConstruction = value;
+	}
+	float getCost()
+	{
+		return costOfConstructionPerUnitOfTime;
+	}
+	void showInfoBuildCompany()
+	{
+		cout << "Building company: " << getNameBuildingCompany() 
+			<< " cost: " << getCost() << " and have time: " << getTime() << "\n";
+	}
 };
 
-class MetroBuildCompany : public BuildingCompany, public InsuranceProvider
+class MetroBuildCompany : public BuildingCompany
 {
 	string nameOfBuildedObject;
-	float howMuchBudgetMoneyIsSpent;
+	float spent;
 	float distant;
 
 public:
@@ -106,10 +185,75 @@ public:
 	{
 
 	}
+
+	string getNameOfObject()
+	{
+		return nameOfBuildedObject;
+	}
+
+	void setNameOfObject(string name)
+	{
+		this->nameOfBuildedObject = name;
+	}
+
+	float getDistance()
+	{
+		return distant;
+	}
+	void setDistance(float value)
+	{
+		this->distant = value;
+	}
+
+	float isSpent( float cost)
+	{
+		spent = distant * cost;
+		return spent;
+	}
+
+	void showInfoBuidedObject()
+	{
+		cout << "Builded object: " << getNameOfObject() 
+			<< " have distasce: " << getDistance() << " and it has been spent on him " << spent << "\n";
+	}
 };
 
 int main()
 {
-	MetroBuildCompany newBranch();
+	float compensationDef = 0, compensation = 0, cost = 0, costDef = 0;
+	float budgetDef = 0, budget = 0;
 
+	Organisation organisation, organisationConstructed("Conglomerate", 2000000.1);
+	InsuranceProvider insProvider, insProviderConstructed("Unreliable", 1000000.0);
+	BuildingCompany buildCompany, buildCompanyConstructed("DefBuilder", 62);
+	MetroBuildCompany newBranch, newBranchConstructed("NUBiP-Washington", 7365.23);
+
+	compensationDef = insProvider.getBudgetLosses() * insProvider.getInsPaiment();
+	compensation = insProviderConstructed.getBudgetLosses() * insProviderConstructed.getInsPaiment();
+
+	costDef = buildCompany.getTime() * buildCompany.getCost();
+	cost = buildCompanyConstructed.getTime() * buildCompanyConstructed.getCost();
+	
+	budgetDef = organisation.getBudget() + compensationDef;
+	budget = organisationConstructed.getBudget() + compensation;
+
+	cout << "New branch: " << newBranch.getNameOfObject() << "is costed: " << newBranch.isSpent(costDef);
+	cout << "\nNew branch: " << newBranchConstructed.getNameOfObject() << "is costed: " << newBranchConstructed.isSpent(cost);
+
+	cout << "\n---------------\nAll info\n-----------------\n";
+	cout << "Defoult object\n";
+	organisation.showInfoOrganisation();
+	insProvider.showInfoProvider();
+	buildCompany.showInfoBuildCompany();
+	newBranch.showInfoBuidedObject();
+	cout << "\nConstructed object\n";
+	organisationConstructed.showInfoOrganisation();
+	insProviderConstructed.showInfoProvider();
+	buildCompanyConstructed.showInfoBuildCompany();
+	newBranchConstructed.showInfoBuidedObject();
+
+	cout << "\nOrganisation: " << organisation.getNameOrg() << " incurred losses(or no losses): " << budgetDef - newBranch.isSpent(costDef);
+	cout << "\nOrganisation: " << organisationConstructed.getNameOrg() << " incurred losses(or no losses): " << budget - newBranchConstructed.isSpent(cost);
+
+	return 0;
 }
